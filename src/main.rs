9b6351061a6f53804d519f28379c9e68d5c05585
Michal Dunajski <michaldunajski2505@gmail.com>
2021-04-18 00:47:46 +0200

    Adds debugging settings and TOML usage example

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
 extern crate clap;
 use clap::*;
-use std::{fs::File, io::prelude::*, io::BufReader};
-use serde::{Deserialize};
+use std::{fs::File, io::prelude::*, io::BufReader, io::BufWriter};
+use serde::{Deserialize, Serialize};
+use toml::*;
+
+#[derive(Deserialize, Debug)]
+struct Taskito {
+    text: String,
+}
+
+#[derive(Serialize, Debug)]
+struct Config {
+    ip: String,
+    port: Option<u16>,
+    keys: Keys,
+}
+
+#[derive(Serialize, Debug)]
+struct Keys {
+    github: String,
+    travis: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct TaskList {
+    title: String,
+    task: Task,
+}
 
 #[derive(Deserialize, Debug)]
 struct Task {
-    text: String,
+    subject: String,
+    message: String,
+    number: u16,
 }
+
 
 fn main() -> std::io::Result<()> {
     let matches = App::new("Task Saver")
@@ -31,9 +59,36 @@ fn main() -> std::io::Result<()> {
     let mut buf_reader = BufReader::new(file);
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents)?;
-    // println!("Co tam jest? {}", contents);
+    println!("Co tam jest? {}", contents);
 
-    let task: Task = serde_json::from_str(&contents).unwrap();
+    let task: Taskito = serde_json::from_str(&contents).unwrap();
     println!("task = {:?}", task);
+
+    let config = Config {
+        ip: "127.0.0.1".to_string(),
+        port: None,
+        keys: Keys {
+            github: "xxxxxxxxxxxxxxxxx".to_string(),
+            travis: Some("yyyyyyyyyyyyyyyyy".to_string()),
+        },
+    };
+
+    let toml = toml::to_string(&config).unwrap();
+
+    let mut file = File::create("config.toml")?;
+    file.write(toml.as_bytes())?;
+
+    println!("{:?}", config);
+
+    ////////////////////////
+    let file = File::open("tasks.toml")?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+
+    let task: TaskList = toml::from_str(&contents).unwrap();
+
+    println!("{:?}", task);
+
     Ok(())
 }
