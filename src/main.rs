90def41139d304720a1f546b1157e426af717996
Michal Dunajski <michaldunajski2505@gmail.com>
2021-04-17 19:16:30 +0200

    Example with reading from JSon and argument
    - Used clap (thanks Rust community for that advice) for basic of file
      usage, --help, arguments to invoke.
    
    - Added --show flag to show all tasks(not implemented yet)
    
    - Used serde to open JSon file and parse data into struct Task

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 extern crate clap;
 use clap::*;
+use std::{fs::File, io::prelude::*, io::BufReader};
+use serde::{Deserialize};
 
-fn main() {
+#[derive(Deserialize, Debug)]
+struct Task {
+    text: String,
+}
+
+fn main() -> std::io::Result<()> {
     let matches = App::new("Task Saver")
         .version("1.0")
         .author("dnj")
@@ -17,6 +24,16 @@ fn main() {
     // matches.is_present("s")
     match matches.occurrences_of("show") {
         1 => println!("show tasks!"),
-        _ => println!("I have to do nothing :<"),
+        _ => println!("I have nothing to do :<"),
     }
+
+    let file = File::open("tasks.json")?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    // println!("Co tam jest? {}", contents);
+
+    let task: Task = serde_json::from_str(&contents).unwrap();
+    println!("task = {:?}", task);
+    Ok(())
 }
